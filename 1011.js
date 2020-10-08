a78bd9dccbbfd3f1b045f5d85c4e5e99d3b4fb1a
Caio César <51300251+caiocsp@users.noreply.github.com>
2020-10-08 18:00:29 -0300

    Update 1011.js

--- 1011.js
+++ 1011.js
@@ -4,6 +4,6 @@ var lines = input.split('\n');
 var pi = 3.14159;
 var raio = parseFloat(lines.shift());
 
-var vol = (4/3.0) * pi * (raio * raio * raio);
+var vol = (4/3.0) * pi * Math.pow(raio, 3);
 
 console.log("VOLUME = " + vol.toFixed(3));
